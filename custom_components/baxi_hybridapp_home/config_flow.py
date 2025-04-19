9c96599a3637c41a35283cbfe50ec8b33df3ea82
Ale <stef.alexander@gmail.com>
2025-04-19 22:56:20 +0200

    Create config_flow.py

---
+++ custom_components/baxi_hybridapp_home/config_flow.py
@@ -0,0 +1,24 @@
+from homeassistant import config_entries
+import voluptuous as vol
+from .const import DOMAIN
+
+CONFIG_SCHEMA = vol.Schema({
+    vol.Required("username"): str,
+    vol.Required("password"): str,
+})
+
+class BaxiHybridAppHomeFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+    """Gestione del flusso di configurazione per Baxi HybridApp Home."""
+
+    async def async_step_user(self, user_input=None):
+        """Primo step di configurazione, richiede credenziali utente."""
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Baxi HybridApp Home",
+                data=user_input
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=CONFIG_SCHEMA
+        )
